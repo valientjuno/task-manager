bbc618b97a537b7095a09e6f2d564e42ab55c887
valientjuno <jessedoake91@gmail.com>
2025-08-27 12:40:10 -0600

    half way done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,35 @@
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const Task = require("../models/Task");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const getTask = (req, res) => {
-  res.send("get single task");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.stats(404).json({ msg: `NO task with id : ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.stats(500).json({ msg: error });
+  }
 };
+
 const updateTask = (req, res) => {
   res.send("update task");
 };
